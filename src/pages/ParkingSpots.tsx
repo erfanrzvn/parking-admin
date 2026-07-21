b194b0b280ad7b18030b7bfa3e32ca2e6ed36abc
Erfan <erfanrezvani@yahoo.com>
2026-07-21 22:22:24 +0330

    Fix schema import paths in ParkingSpots and Parkings

--- src/pages/ParkingSpots.tsx
+++ src/pages/ParkingSpots.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { generateClient } from 'aws-amplify/data';
-import type { Schema } from '../amplify/data/resource';
+import type { Schema } from '../schema';
 import './ParkingSpots.css';
 
 const client = generateClient<Schema>();
